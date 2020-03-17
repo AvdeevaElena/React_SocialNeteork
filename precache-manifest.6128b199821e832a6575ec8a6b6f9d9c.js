c177500d6d73132c9cc4c8fc41025744daa6ad44
AvdeevaElena <avdeevaelena5@gmail.com>
2020-03-17 21:46:35 +0300

    Updates

---
+++ precache-manifest.6128b199821e832a6575ec8a6b6f9d9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09b6523cb07b9ae95a857e376c58b10",
+    "url": "/React_SocialNeteork/index.html"
+  },
+  {
+    "revision": "1ae1e7e3524f7240587b",
+    "url": "/React_SocialNeteork/static/css/main.d5c5f5d7.chunk.css"
+  },
+  {
+    "revision": "5c2d11b9c236b70a6b96",
+    "url": "/React_SocialNeteork/static/js/2.f727172e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/React_SocialNeteork/static/js/2.f727172e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae1e7e3524f7240587b",
+    "url": "/React_SocialNeteork/static/js/main.79e3eb89.chunk.js"
+  },
+  {
+    "revision": "77181cc2175c8332a2e2",
+    "url": "/React_SocialNeteork/static/js/runtime-main.4ecb0d39.js"
+  },
+  {
+    "revision": "af6fc50c807405ddc5e2d033befc38d2",
+    "url": "/React_SocialNeteork/static/media/preloader.af6fc50c.svg"
+  },
+  {
+    "revision": "584ed1c6ba7d7cfbe3b076992d3d7c82",
+    "url": "/React_SocialNeteork/static/media/user.584ed1c6.png"
+  }
+]);
